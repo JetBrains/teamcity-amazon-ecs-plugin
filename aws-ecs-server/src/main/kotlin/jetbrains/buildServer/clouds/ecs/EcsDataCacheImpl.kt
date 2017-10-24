5d008a52c0ea37a62f2b42a67917d7aab1c771db
ekoshkin <koshkinev@gmail.com>
2017-10-24 11:36:24 +0300

    make EcsData cache expirable

--- aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsDataCacheImpl.kt
+++ aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsDataCacheImpl.kt
@@ -1,12 +1,26 @@
 package jetbrains.buildServer.clouds.ecs
 
 import jetbrains.buildServer.clouds.InstanceStatus
+import jetbrains.buildServer.serverSide.TeamCityProperties
 import java.util.concurrent.ConcurrentHashMap
 
+val CACHE_EXPIRATION_TIMEOUT_PROPERTY = "teamcity.ecs.cache.expirationTimeout"
+
 class EcsDataCacheImpl : EcsDataCache {
-    private val cache: MutableMap<String, InstanceStatus> = ConcurrentHashMap()
+    private val entryExpirationTimeout: Int = TeamCityProperties.getInteger(CACHE_EXPIRATION_TIMEOUT_PROPERTY, 10 * 1000)
+
+    private val cache: MutableMap<String, CacheEntry<InstanceStatus>> = ConcurrentHashMap()
 
     override fun getInstanceStatus(taskArn: String, resolver: () -> InstanceStatus): InstanceStatus {
-        return cache.getOrPut(taskArn, resolver)
+        val now = System.currentTimeMillis()
+        synchronized(cache){
+            val cacheEntry = cache.get(taskArn)
+            if(cacheEntry == null || (now - cacheEntry.timestamp) > entryExpirationTimeout){
+                cache.put(taskArn, CacheEntry(now, resolver.invoke()))
+            }
+        }
+        return cache.get(taskArn)!!.data
     }
+
+    class CacheEntry<T>(val timestamp: Long, val data: T)
 }
---
+++ aws-ecs-server/src/test/kotlin/jetbrains/buildServer/clouds/ecs/EcsDataCacheTest.kt
@@ -0,0 +1,66 @@
+package jetbrains.buildServer.clouds.ecs
+
+import jetbrains.buildServer.BaseTestCase
+import jetbrains.buildServer.clouds.InstanceStatus
+import junit.framework.Assert
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+
+/**
+ * Created by Evgeniy Koshkin (evgeniy.koshkin@jetbrains.com) on 24.10.17.
+ */
+@Test
+class EcsDataCacheTest : BaseTestCase() {
+    private lateinit var cache: EcsDataCache
+
+    @BeforeMethod
+    override fun setUp() {
+        super.setUp()
+        setInternalProperty(CACHE_EXPIRATION_TIMEOUT_PROPERTY, 50.toString())
+        cache = EcsDataCacheImpl()
+    }
+
+    @AfterMethod
+    override fun tearDown() {
+        super.tearDown()
+    }
+
+    @Test
+    fun testGetInstanceStatusShouldCache() {
+        var invocationCount = 0
+        val resolver = {
+            invocationCount++
+            InstanceStatus.RUNNING
+        }
+        Assert.assertEquals(InstanceStatus.RUNNING, cache.getInstanceStatus("foo", resolver))
+        Assert.assertEquals(InstanceStatus.RUNNING, cache.getInstanceStatus("foo", resolver))
+        Assert.assertEquals(InstanceStatus.RUNNING, cache.getInstanceStatus("boo", resolver))
+        Assert.assertEquals(2, invocationCount)
+    }
+
+    @Test
+    fun testGetInstanceStatusShouldExpires() {
+        var invocationCount = 0
+        val resolver = {
+            invocationCount++
+            when(invocationCount){
+                1 -> InstanceStatus.STARTING
+                2 -> InstanceStatus.RUNNING
+                3 -> InstanceStatus.STOPPING
+                4 -> InstanceStatus.STOPPED
+                else -> InstanceStatus.UNKNOWN
+            }
+        }
+        Assert.assertEquals(InstanceStatus.STARTING, cache.getInstanceStatus("foo", resolver))
+        Assert.assertEquals(InstanceStatus.STARTING, cache.getInstanceStatus("foo", resolver))
+        Assert.assertEquals(InstanceStatus.STARTING, cache.getInstanceStatus("foo", resolver))
+        Thread.sleep(100)
+        Assert.assertEquals(InstanceStatus.RUNNING, cache.getInstanceStatus("foo", resolver))
+        Assert.assertEquals(InstanceStatus.RUNNING, cache.getInstanceStatus("foo", resolver))
+        Thread.sleep(100)
+        Assert.assertEquals(InstanceStatus.STOPPING, cache.getInstanceStatus("foo", resolver))
+        Thread.sleep(100)
+        Assert.assertEquals(InstanceStatus.STOPPED, cache.getInstanceStatus("foo", resolver))
+    }
+}

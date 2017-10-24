d3881333e15ddbb43697a5d26e87078afe316a59
ekoshkin <koshkinev@gmail.com>
2017-10-24 17:38:12 +0300

    re-read "teamcity.ecs.cache.expirationTimeout" property every time

--- aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsDataCacheImpl.kt
+++ aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsDataCacheImpl.kt
@@ -7,15 +7,13 @@ import java.util.concurrent.ConcurrentHashMap
 val CACHE_EXPIRATION_TIMEOUT_PROPERTY = "teamcity.ecs.cache.expirationTimeout"
 
 class EcsDataCacheImpl : EcsDataCache {
-    private val entryExpirationTimeout: Int = TeamCityProperties.getInteger(CACHE_EXPIRATION_TIMEOUT_PROPERTY, 10 * 1000)
-
     private val cache: MutableMap<String, CacheEntry<InstanceStatus>> = ConcurrentHashMap()
 
     override fun getInstanceStatus(taskArn: String, resolver: () -> InstanceStatus): InstanceStatus {
         val now = System.currentTimeMillis()
         synchronized(cache){
             val cacheEntry = cache.get(taskArn)
-            if(cacheEntry == null || (now - cacheEntry.timestamp) > entryExpirationTimeout){
+            if(cacheEntry == null || (now - cacheEntry.timestamp) > TeamCityProperties.getInteger(CACHE_EXPIRATION_TIMEOUT_PROPERTY, 10 * 1000)){
                 cache.put(taskArn, CacheEntry(now, resolver.invoke()))
             }
         }

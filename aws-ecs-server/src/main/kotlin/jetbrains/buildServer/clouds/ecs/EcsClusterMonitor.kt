2c4505c65ae1d2b98cd7e6015395bff8361f8fba
ekoshkin <koshkinev@gmail.com>
2017-11-13 16:20:48 +0300

    cleanup

--- aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsClusterMonitor.kt
+++
@@ -1,8 +0,0 @@
-package jetbrains.buildServer.clouds.ecs
-
-/**
- * Created by Evgeniy Koshkin (evgeniy.koshkin@jetbrains.com) on 01.11.17.
- */
-interface EcsClusterMonitor {
-    fun clusterHasAvailableResources(): Boolean
-}

0440028fc90df0b4cf27271a407c07765f25d18a
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2018-02-15 18:52:27 +0100

    apply all passed custom agent parameters as config parameters regardless parameter name prefix

--- aws-ecs-agent/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsAgentConfigurationProvider.kt
+++ aws-ecs-agent/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsAgentConfigurationProvider.kt
@@ -4,8 +4,6 @@ import jetbrains.buildServer.agent.AgentLifeCycleAdapter
 import jetbrains.buildServer.agent.AgentLifeCycleListener
 import jetbrains.buildServer.agent.BuildAgent
 import jetbrains.buildServer.agent.BuildAgentConfigurationEx
-import jetbrains.buildServer.agent.Constants.ENV_PREFIX
-import jetbrains.buildServer.agent.Constants.SYSTEM_PREFIX
 import jetbrains.buildServer.util.EventDispatcher
 import jetbrains.buildServer.util.StringUtil
 
@@ -28,19 +26,11 @@ class EcsAgentConfigurationProvider(agentEvents: EventDispatcher<AgentLifeCycleL
         val providedServerUrl = environment[SERVER_URL_ECS_ENV]
         if (!StringUtil.isEmpty(providedServerUrl)) agentConfigurationEx.serverUrl = providedServerUrl
 
-        val profileId = environment[PROFILE_ID_ECS_ENV]
-        if (!StringUtil.isEmpty(profileId)) agentConfigurationEx.addSystemProperty(REQUIRED_PROFILE_ID_SYS_PROP, profileId!!)
-
         environment.entries.forEach { entry ->
             val key = entry.key
             val value = entry.value
             if (key.startsWith(TEAMCITY_ECS_PROVIDED_PREFIX)){
-                val parameterName = key.removePrefix(TEAMCITY_ECS_PROVIDED_PREFIX)
-                when {
-                    parameterName.startsWith(SYSTEM_PREFIX) -> agentConfigurationEx.addSystemProperty(parameterName.removePrefix(SYSTEM_PREFIX), value)
-                    parameterName.startsWith(ENV_PREFIX) -> agentConfigurationEx.addEnvironmentVariable(parameterName.removePrefix(ENV_PREFIX), value)
-                    else -> agentConfigurationEx.addConfigurationParameter(parameterName, value)
-                }
+                agentConfigurationEx.addConfigurationParameter(key.removePrefix(TEAMCITY_ECS_PROVIDED_PREFIX), value)
             }
         }
     }

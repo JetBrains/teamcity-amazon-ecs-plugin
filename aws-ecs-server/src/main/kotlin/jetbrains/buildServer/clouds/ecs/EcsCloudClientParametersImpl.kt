50f39e260f7d97e0c795eb35ce0fdbaca560556c
Sergey Pak <sergey.pak@jetbrains.com>
2021-08-11 02:09:15 +0200

    use creds from AWSCommonParams

--- aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsCloudClientParametersImpl.kt
+++ aws-ecs-server/src/main/kotlin/jetbrains/buildServer/clouds/ecs/EcsCloudClientParametersImpl.kt
@@ -47,32 +47,11 @@ class EcsCloudClientParametersImpl(private val genericParams: CloudClientParamet
 }
 
 fun Map<String, String>.toAwsCredentials(): AWSCredentials? {
-    val accessKeyId = getAccessKeyId(this)
-    val secretAccessKey = getSecretAccessKey(this)
-    val useDefaultCredProvChain = isUseDefaultCredentialProviderChain(this)
-    return if(useDefaultCredProvChain)
-        null
-    else
-        BasicAWSCredentials(accessKeyId, secretAccessKey)
-}
-
-private fun isUseDefaultCredentialProviderChain(params: Map<String, String>): Boolean {
-    return java.lang.Boolean.parseBoolean(params[USE_DEFAULT_CREDENTIAL_PROVIDER_CHAIN_PARAM]) || java.lang.Boolean.parseBoolean(USE_DEFAULT_CREDENTIAL_PROVIDER_CHAIN_PARAM_OLD)
-}
-
-private fun getAccessKeyId(params: Map<String, String>): String? {
-    return getNewOrOld(params, ACCESS_KEY_ID_PARAM, ACCESS_KEY_ID_PARAM_OLD)
-}
-
-private fun getSecretAccessKey(params: Map<String, String>): String? {
-    var secretAccessKeyParam = params[SECURE_SECRET_ACCESS_KEY_PARAM]
-    if (StringUtil.isNotEmpty(secretAccessKeyParam)) return secretAccessKeyParam
-
-    secretAccessKeyParam = params[SECURE_SECRET_ACCESS_KEY_PARAM_OLD]
-    return if (StringUtil.isNotEmpty(secretAccessKeyParam)) secretAccessKeyParam else params[SECRET_ACCESS_KEY_PARAM_OLD]
-}
-
-private fun getNewOrOld(params: Map<String, String>, newKey: String, oldKey: String): String? {
-    val newVal = params[newKey]
-    return if (StringUtil.isNotEmpty(newVal)) newVal else params[oldKey]
+    try {
+        val credentialsProvider = getCredentialsProvider(this)
+        return credentialsProvider.credentials
+    } catch (ex:Exception) {
+        ex.printStackTrace()
+        return null
+    }
 }
